a356fc95fcbd504ab91cb46a0aab1fac67d8a962
Thawin06 <thawinmekchai@gmail.com>
2026-07-24 19:10:36 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+/*****************************************
+ * Student : 69160126 Thawin Mekchai
+ * Program : MyFirstProgram
+ * Description : Personal student profile
+ ******************************************/
+public class Main {
+    public static void main(String[] args){
+        System.out.println("******************************");
+        System.out.println("* My name is : Thawin Mekchai  *");
+        System.out.println("* Student Id : 69160126        *");
+        System.out.print("******************************");
+
+
+    }
+}
